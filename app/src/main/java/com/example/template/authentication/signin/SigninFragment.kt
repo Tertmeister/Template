ae96dc4521ac37fa5b27aa09e9b9040d0254f08c
Bob Saget <terry.n.enzweiler@gmail.com>
2022-07-24 16:06:06 -0500

    handled enabling and disabling of sign in button

--- app/src/main/java/com/example/template/authentication/signin/SigninFragment.kt
+++ app/src/main/java/com/example/template/authentication/signin/SigninFragment.kt
@@ -6,6 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.core.widget.addTextChangedListener
 import com.example.template.R
 
 class SigninFragment : Fragment() {
@@ -15,16 +18,36 @@ class SigninFragment : Fragment() {
     }
 
     private lateinit var viewModel: SigninViewModel
+    private lateinit var mSigninButton: Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.sign_in_fragment, container, false)
+        val view= inflater.inflate(R.layout.sign_in_fragment, container, false)
+        mSigninButton=view.findViewById(R.id.sign_in_btn)
+
+        view.findViewById<EditText>(R.id.email_et).addTextChangedListener {
+          it?.let {
+              viewModel.updateEmail(it.trim().toString())
+              it.trim().toString()
+          }
+        }
+        view.findViewById<EditText>(R.id.password_et).addTextChangedListener {
+            it?.let {
+                viewModel.updatePassword(it.trim().toString())
+            }
+        }
+
+        return view
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(SigninViewModel::class.java)
+
+        viewModel.isSignInButtonEnabled.observe(viewLifecycleOwner) {
+            mSigninButton.isEnabled = it
+        }
     }
 }
